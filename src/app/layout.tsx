c6eaec98692af4d5ea7f248c6c510a86ae95f7b8
Srichand Kaushik <srichand.kaushik@gmail.com>
2026-07-22 14:22:19 +0530

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,16 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>       <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ECT9ECZ2XY"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+      <head>
+        <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-ECT9ECZ2XY"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-          gtag('config', 'G-ECT9ECZ2XY');
-        </script>
-        <meta charset="utf-8">
+            gtag('config', 'G-ECT9ECZ2XY');
+          </script>
       </head>
       <body suppressHydrationWarning>{children}</body>
     </html>
